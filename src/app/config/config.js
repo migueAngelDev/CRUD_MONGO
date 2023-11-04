8f0c247e521abb23e1d654ad8de58cf45836c92d
migueAngelDev <angeldev1@outlook.com>
2023-11-04 16:54:58 -0600

    add config db

---
+++ src/app/config/config.js
@@ -0,0 +1,5 @@
+import { config } from "dotenv";
+
+config();
+
+export const MONGODB = process.env.MONGODB_URI;
--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -1,12 +1,14 @@
 import { connect, connection } from "mongoose";
 
+import { MONGODB } from "@/app/config/config";
+
 const conn = {
   isConnected: false,
 };
 
 export async function connectDB() {
   if (conn.isConnected) return;
-  const db = await connect("mongodb://localhost/nextmongocrud");
+  const db = await connect(MONGODB);
   console.log(db.connection.db.databaseName);
   conn.isConnected = db.connections[0].readyState;
 }
