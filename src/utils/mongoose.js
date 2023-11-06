5a65941433bf0db4faddf7575e9ed32965407f8d
migueAngelDev <angeldev1@outlook.com>
2023-11-05 18:12:58 -0600

    config formUsers

--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -1,6 +1,8 @@
 import { connect, connection } from "mongoose";
+// import { MONGODB } from "@/app/config/config";
 
-import { MONGODB } from "@/app/config/config";
+const MONGODB =
+  "mongodb+srv://vercel-admin-user:futurama@my-mongodb-mobile.o1kbqqc.mongodb.net/?retryWrites=true&w=majority";
 
 const conn = {
   isConnected: false,
@@ -8,7 +10,10 @@ const conn = {
 
 export async function connectDB() {
   if (conn.isConnected) return;
-  const db = await connect(MONGODB);
+  const db = await connect(MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   console.log(db.connection.db.databaseName);
   conn.isConnected = db.connections[0].readyState;
 }
