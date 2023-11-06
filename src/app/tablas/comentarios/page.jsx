5d3db0acdbb228642e4a31ae5167f0da2fe24e84
migueAngelDev <angeldev1@outlook.com>
2023-11-05 18:38:23 -0600

    config formUsers

--- src/app/tablas/comentarios/page.jsx
+++ src/app/tablas/comentarios/page.jsx
@@ -2,10 +2,40 @@ import styles from "./comentarios.module.css";
 import { connectDB } from "@/utils/mongoose";
 import Comment from "@/models/Comment";
 
+// const loadComment = async () => {
+//   // connectDB();
+
+//   try {
+//     const comments = await fetch("/api/comments");
+//     console.log(comments);
+//     return comments;
+//   } catch (error) {
+//     console.log(error);
+//     console.log(error.message);
+//   }
+//   // const comments = await Comment.find();
+// };
+
 const loadComment = async () => {
-  // connectDB();
-  const comments = await Comment.find();
-  return comments;
+  try {
+    // Realiza la solicitud GET a la ruta /api/comments y espera la respuesta
+    const response = await fetch(
+      "https://mongodb-azure.vercel.app/api/comments"
+    );
+
+    // Verifica si la respuesta es exitosa (código de estado 200)
+    if (response.status === 200) {
+      // Convierte la respuesta a JSON para obtener los datos
+      const comments = await response.json();
+      return comments;
+    } else {
+      console.log("Error al obtener datos. Código de estado:", response.status);
+      return []; // Otra acción en caso de error, por ejemplo, retornar un array vacío
+    }
+  } catch (error) {
+    console.log("Error en la solicitud:", error);
+    return []; // Manejar errores de red
+  }
 };
 
 const TableComments = async () => {
